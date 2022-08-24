ab0fba556338fc8de8b35571300a6b133617611a
korekishor <101495100+korekishor@users.noreply.github.com>
2022-08-24 12:52:55 +0530

    spacy

---
+++ nltk1.py
@@ -0,0 +1,155 @@
+text = """Founded in 2002, SpaceX’s mission is to enable humans to become a spacefaring civilization and a multi-planet 
+species by building a self-sustaining city on Mars. In 2008, SpaceX’s Falcon 1 became the first privately developed 
+liquid-fuel launch vehicle to orbit the Earth."""
+# Splits at space 
+text.split()
+text.split('. ')
+import re 
+sentences = re.compile('[.!?] ').split(text)
+
+from nltk.tokenize import word_tokenize 
+# word_tokenize(text)
+# Output=['Founded', 'in', '2002', ',', 'SpaceX', '’', 's', 'mission', 'is', 'to', 'enable', 
+#          'humans', 'to', 'become', 'a', 'spacefaring', 'civilization', 'and', 'a', 
+#          'multi-planet', 'species', 'by', 'building', 'a', 'self-sustaining', 'city', 'on', 
+#          'Mars', '.', 'In', '2008', ',', 'SpaceX', '’', 's', 'Falcon', '1', 'became', 
+#          'the', 'first', 'privately', 'developed', 'liquid-fuel', 'launch', 'vehicle', 
+#          'to', 'orbit', 'the', 'Earth', '.']
+
+
+from nltk.tokenize import sent_tokenize
+# sent_tokenize(text)
+# Output= ['Founded in 2002, SpaceX’s mission is to enable humans to become a spacefaring 
+#           civilization and a multi-planet \nspecies by building a self-sustaining city on 
+#           Mars.', 
+#          'In 2008, SpaceX’s Falcon 1 became the first privately developed \nliquid-fuel 
+#           launch vehicle to orbit the Earth.]
+
+
+from spacy.lang.en import English
+nlp = English()
+my_doc = nlp(text)
+
+# Create list of word tokens
+token_list = []
+for token in my_doc:
+    token_list.append(token.text)
+token_list
+# Output = ['Founded', 'in', '2002', ',', 'SpaceX', '’s', 'mission', 'is', 'to', 'enable', 
+#           'humans', 'to', 'become', 'a', 'spacefaring', 'civilization', 'and', 'a', 
+#           'multi', '-', 'planet', '\n', 'species', 'by', 'building', 'a', 'self', '-', 
+#           'sustaining', 'city', 'on', 'Mars', '.', 'In', '2008', ',', 'SpaceX', '’s', 
+#           'Falcon', '1', 'became', 'the', 'first', 'privately', 'developed', '\n', 
+#           'liquid', '-', 'fuel', 'launch', 'vehicle', 'to', 'orbit', 'the', 'Earth', '.']
+
+
+
+# Load English tokenizer, tagger, parser, NER and word vectors
+nlp = English()
+
+# Create the pipeline 'sentencizer' component
+sbd = nlp.create_pipe('sentencizer')
+
+# Add the component to the pipeline
+# nlp.add_pipe(sbd)
+
+# doc = nlp(text)
+
+# create list of sentence tokens
+# sents_list = []
+# for sent in doc.sents:
+#     sents_list.append(sent.text)
+# sents_list
+
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+  
+ps = PorterStemmer()
+  
+# choose some words to be stemmed
+words = ["program", "programs", "programmer", "programming", "programmers"]
+  
+for w in words:
+    # print(w, " : ", ps.stem(w))
+    pass
+
+#  output=program program program program program program  ----- this splite in the base root 
+# sentence = "Programmers program with programming languages"
+#  output =  program program wit program language 
+
+
+
+from nltk.stem import WordNetLemmatizer
+  
+lemmatizer = WordNetLemmatizer()
+  
+# print("rocks :", lemmatizer.lemmatize("rocks"))
+# print("corpora :", lemmatizer.lemmatize("corpora"))
+  
+# a denotes adjective in "pos"
+# print("better :", lemmatizer.lemmatize("better", pos ="a"))
+
+# output: rocks : rock
+# corpora : corpus
+# better : good
+
+import nltk
+# nltk.download()
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+example_sent = """This is a sample sentence,
+				showing off the stop words filtration."""
+
+stop_words = set(stopwords.words('English'))
+ 
+word_tokens = word_tokenize(example_sent)
+  
+filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+ 
+filtered_sentence = []
+
+for w in word_tokens:
+	if w not in stop_words:
+		filtered_sentence.append(w)
+
+
+# print(word_tokens)
+# print(filtered_sentence)
+
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+stop_words = set(stopwords.words('english'))
+# pos_tag() --- give us type of world is verb,noun,adj
+# // Dummy text
+
+txt = "Sukanya, Rajib and Naba are my good friends. " \
+    "Sukanya is getting married next year. " \
+    "Marriage is a big step in one’s life." \
+    "It is both exciting and frightening. " \
+    "But friendship is a sacred bond between people." \
+    "It is a special kind of love between us. " \
+    "Many of you must have tried searching for a friend "\
+    "but never found the right one."
+ 
+# sent_tokenize is one of instances of use for divide sentenses
+# PunktSentenceTokenizer from the nltk.tokenize.punkt module
+ 
+tokenized = sent_tokenize(txt)
+for i in tokenized:
+     
+    # Word tokenizers is used to find the words
+    # and punctuation in a string
+    wordsList = nltk.word_tokenize(i)
+ 
+    # removing stop words from wordList
+    wordsList = [w for w in wordsList if not w in stop_words]
+ 
+    #  Using a Tagger. Which is part-of-speech
+    # tagger or POS-tagger.
+    tagged = nltk.pos_tag(wordsList)
+ 
+    # print(tagged)
---
+++ spacy_1.py
@@ -0,0 +1,85 @@
+# import spacy 
+# names entity recorganisation
+# text recorganisation tool
+# mahcine translations procees all documnet is all for the nlp 
+# classification and information extractions 
+
+# nlp=spacy.load("en_core_web_sm")
+# text="""text added for data information"""
+# # print(nlp(text))
+
+# with open('data/wiki_us.text','r') as f:
+#     text=f.read()
+
+# print(text)
+# -------------------------------------------
+# import spacy
+# nlp = spacy.load("en_core_web_sm")
+# doc = nlp("Welcome to the Data Science Learner! . Here you will learn all things about data science , machine learning , artifical intelligence and more." )
+# empty_list = []
+# for token in doc:
+#     empty_list.append(token.lemma_)
+
+# final_string = ' '.join(map(str,empty_list))
+# print(final_string)
+# -----------------------------------------
+# pip install -U spacy
+# python -m spacy download en_core_web_sm
+
+
+# import spacy
+
+# # Load English tokenizer, tagger, parser and NER
+# nlp = spacy.load("en_core_web_sm")
+
+# # Process whole documents
+# text = ("When Sebastian Thrun started working on self-driving cars at "
+#         "Google in 2007, few people outside of the company took him "
+#         "seriously. “I can tell you very senior CEOs of major American "
+#         "car companies would shake my hand and turn away because I wasn’t "
+#         "worth talking to,” said Thrun, in an interview with Recode earlier "
+#         "this week.")
+# doc = nlp(text)
+
+# # doc.noun_chunks-- geting all phrases from text
+# # Analyze syntax
+# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+
+# # token.lemma_ -- geting all verb from text
+# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+# # Find named entities, phrases and concepts
+# # entity.text, entity.label_ ----- as person names, organizations, locations, medical codes, time expressions, quantities, monetary values, percentages
+# for entity in doc.ents:
+#     print(entity.text, entity.label_)
+ 
+# ----------------------
+
+def set_custom1(doc):
+    for token in doc:
+        print(token,"--------",token.i)
+        
+def set_custom2(doc):
+    for token in doc[:-1]:
+        if token.text==';':
+            doc[token.i+1].is_sent_start=True
+        return doc
+
+
+import spacy
+nlp=spacy.load('en_core_web_sm')
+text=nlp(u"When Sebastian Thrun started working on self-driving cars at "
+         "Google in 2007, few people outside of the ; company took him "
+         "seriously. “I can tell you very senior CEOs of major American "
+         "car companies would shake my hand and turn away because I wasn’t "
+        "worth talking to,” said Thrun, in an interview with Recode earlier "
+         "this week.")
+
+
+# for lines in text.sents:
+#     print(lines)
+
+print("---------000000") 
+ 
+nlp.add_pipe()
+print(nlp.add_pipe_names)
